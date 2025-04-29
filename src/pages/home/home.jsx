e104d5e0ca4427476683782cfde122e073064e01
shahnoow <shahnawazjawed9@gmail.com>
2025-04-30 00:09:33 +0500

    Initial commit

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,6 +1,8 @@
 import Featured from "../../components/featured/Featured.jsx";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties.jsx";
 import Header from "../../components/header/Header.jsx";
 import Nabar from "../../components/nabar/Nabar.jsx";
+import PropertyList from "../../components/propertyList/PropertyList.jsx";
 import "./home.css";
 
 const Home = () => {
@@ -10,7 +12,10 @@ const Home = () => {
       <Header />
       <div className="homeContainer">
         <Featured />
-        <Featured />
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList />
+        <h1 className="homeTitle">Homes guests love</h1>
+        <FeaturedProperties />
       </div>
     </div>
   );
