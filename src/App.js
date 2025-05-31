85bfc61925e0e4817e5623aa4905714236f2409f
shahnoow <shahnawazjawed9@gmail.com>
2025-05-31 16:05:05 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import List from "./pages/list/List.jsx";
 // import Hotel from "./pages/hotel/Hotel.jsx";
 
-import Home from "./pages/home/Home_temp.jsx";
-import List from "./pages/list/List.jsx";
-import Hotel from "./pages/hotel/Hotel.jsx";
+import Home from "./pages/home/Home_temp";
+import List from "./pages/list/List";
+import Hotel from "./pages/hotel/Hotel";
 
 export default function App() {
   return (
